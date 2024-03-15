203735b05a9b20e95faeb24b4a3447e879bf4ee9
Leonardo Duarte Veiga Ferreira <leonardoveiga010@gmail.com>
2024-03-15 19:48:10 -0300

    Create variaveisOperadores.js

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+const num_A = 1;
+const num_B = 1;
+
+const soma = num_A + num_B;
+const sub = num_A - num_B;
+const multi = num_A * num_B;
+const div = num_A / num_B;
+
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é de ${soma}`);
+console.log(`Resultado da subtracao entre ${num_A} e ${num_B} é de ${sub}`);
+console.log(`Resultado da multiplicacao entre ${num_A} e ${num_B} é de ${multi}`);
+console.log(`Resultado da divisao entre ${num_A} e ${num_B} é de ${div}`);
